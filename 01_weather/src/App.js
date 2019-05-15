0b41ecb5a78c10b7e91dad2535d80745e7288fde
Sam <sam.d.odonnell@gmail.com>
2019-05-15 12:14:28 -0700

    Add boilerplate files

---
+++ 01_weather/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Day from './day';
+
+function App() {
+  return (
+    <div className="App">
+      <Day />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01_weather/src/day.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Weather() {
+  return (
+    <div>
+      {/* <img src={icon}/> */}
+
+    </div>
+  )
+}
